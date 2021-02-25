a54fa35aec04ab629d13c13ef6166cec78be28c3
Dan Davison <dandavison7@gmail.com>
2021-02-25 10:46:38 -0500

    Add real script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
-console.log('Hello World');
+#!/usr/bin/env node
+const babel = require("@babel/core");
+const fs = require("fs");
+
+const filename = process.argv[2];
+const code = fs.readFileSync(filename, "utf-8");
+const ast = require("@babel/parser").parse(code, {
+  sourceType: "module",
+  plugins: ["typescript"],
+});
+
+const components: string[] = [];
+const emittedEvents: string[] = [];
+
+const componentsIdentifierVisitor = {
+  Identifier: {
+    enter({ node }: any) {
+      // TODO: collect `components` object key names only
+      if (node.name !== "components") {
+        components.push(node.name);
+      }
+    },
+  },
+};
+
+const output = babel.transformFromAstSync(ast, code, {
+  filename,
+  plugins: [
+    function ComponentGraph() {
+      return {
+        visitor: {
+          Identifier(path: any) {
+            if (path.isIdentifier({ name: "components" })) {
+              path.parentPath.traverse(componentsIdentifierVisitor);
+            } else if (path.node.name.includes("emit")) {
+              let gparentPath = path.parentPath.parentPath;
+              if (gparentPath.isCallExpression()) {
+                emittedEvents.push(gparentPath.node.arguments[0].value);
+              }
+            }
+          },
+        },
+      };
+    },
+  ],
+});
+
+// TODO: nodes are being visited twice
+console.log("components:", new Set(components));
+console.log("emittedEvents:", new Set(emittedEvents));
